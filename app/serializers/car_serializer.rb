e37c44a4f7e34b3d05b1aa90d9d7bd12aea668e7
tacostacostacos <david.epstein@libertymutual.com>
2017-11-28 19:08:45 -0500

    Adjusted car serializer to include make_id
    Added make_id to the car serializer

--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,4 +1,4 @@
 class CarSerializer < ActiveModel::Serializer
-  attributes :id, :model, :year, :label, :user_id, :mileages
+  attributes :id, :model, :year, :label, :user_id, :mileages, :make_id
   has_one :make
 end
